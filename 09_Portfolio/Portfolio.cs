bde7e717f5307204eeac128aca64687f77dc49cd
Donnie <Donnie@DESKTOP-A0TIJCR>
2016-09-27 19:06:39 +0200

    new List....

---
+++ 09_Portfolio/Portfolio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09_Portfolio
+{
+    public class Portfolio 
+    {
+        
+        public Portfolio()
+        {
+
+        }
+        public List<Asset> a = new List<Asset>();
+        public Portfolio(List<Asset> alist)
+        {
+           a = alist;
+        }
+        public double result;
+        public double GetTotalValue()
+        {
+            foreach(Asset element in a)
+            {
+                result += element.GetValue();
+            }
+            return result;
+        }
+
+        public void AddAsset(Asset anobject)
+        {
+
+            a.Add(anobject);
+
+        }
+    }
+}
